797968b10e86fedfb477a0aee655dc2f552ace79
ashleshan <ashlesha.n@247-inc.com>
2014-06-25 22:21:53 +0530

    test commit msg

---
+++ asd2312321/test.sql
@@ -0,0 +1,148 @@
+-- MySQL dump 10.13  Distrib 5.6.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: adp_test
+-- ------------------------------------------------------
+-- Server version	5.6.12
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `app_cms`
+--
+
+DROP TABLE IF EXISTS `app_cms`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `app_cms` (
+  `id` varchar(40) NOT NULL,
+  `app_id` varchar(40) NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `description` varchar(1024) DEFAULT NULL,
+  `params` text,
+  `STATUS` tinyint(1) DEFAULT '1',
+  `defaultCMS` tinyint(1) DEFAULT '0',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_cms_config` (`app_id`),
+  CONSTRAINT `fk_cms_config` FOREIGN KEY (`app_id`) REFERENCES `app_config` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `app_cms`
+--
+
+LOCK TABLES `app_cms` WRITE;
+/*!40000 ALTER TABLE `app_cms` DISABLE KEYS */;
+/*!40000 ALTER TABLE `app_cms` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `app_config`
+--
+
+DROP TABLE IF EXISTS `app_config`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `app_config` (
+  `ID` varchar(40) NOT NULL,
+  `APP_LIB_ID` varchar(40) NOT NULL,
+  `CLIENT_ID` varchar(40) NOT NULL,
+  `URL` text,
+  `GIT_LOCATION` text,
+  `UPDATED_AT` datetime DEFAULT NULL,
+  `CREATED_AT` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `FK_APPLIBRARY` (`APP_LIB_ID`),
+  KEY `FK_APPCLIENT` (`CLIENT_ID`),
+  CONSTRAINT `FK_APPCLIENT` FOREIGN KEY (`CLIENT_ID`) REFERENCES `client` (`ID`),
+  CONSTRAINT `FK_APPLIBRARY` FOREIGN KEY (`APP_LIB_ID`) REFERENCES `app_library` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `app_config`
+--
+
+LOCK TABLES `app_config` WRITE;
+/*!40000 ALTER TABLE `app_config` DISABLE KEYS */;
+INSERT INTO `app_config` VALUES ('ac123','al123','c123','url','git','2013-12-11 16:42:27','2013-12-11 16:42:27');
+/*!40000 ALTER TABLE `app_config` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `app_library`
+--
+
+DROP TABLE IF EXISTS `app_library`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `app_library` (
+  `ID` varchar(40) NOT NULL,
+  `NAME` varchar(100) NOT NULL,
+  `DESCRIPTION` text,
+  `TYPE` varchar(40) DEFAULT NULL,
+  `GIT_LOCATION` text,
+  `CREATED_AT` datetime DEFAULT NULL,
+  `UPDATED_AT` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `NAME` (`NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `app_library`
+--
+
+LOCK TABLES `app_library` WRITE;
+/*!40000 ALTER TABLE `app_library` DISABLE KEYS */;
+INSERT INTO `app_library` VALUES ('al123','lib1','desc12','standard','git','2013-12-11 16:40:40','2013-12-11 16:40:40');
+/*!40000 ALTER TABLE `app_library` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `client` (
+  `ID` varchar(40) NOT NULL DEFAULT '',
+  `NAME` varchar(60) DEFAULT NULL,
+  `CREATED_AT` datetime DEFAULT NULL,
+  `UPDATED_AT` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `client`
+--
+
+LOCK TABLES `client` WRITE;
+/*!40000 ALTER TABLE `client` DISABLE KEYS */;
+INSERT INTO `client` VALUES ('c123','myclient','2013-12-11 16:38:58','2013-12-11 16:38:58');
+/*!40000 ALTER TABLE `client` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-12-11 21:18:56
